93c8a7f221e1e63e5a18f7d4e0c8b39eb914168a
Arjun <arjunmehta1992@gmail.com>
2018-07-21 12:50:24 +0530

    initial commit

---
+++ app/src/main/java/com/app/hiten/loanapplication/network/ApiService.java
@@ -0,0 +1,26 @@
+package com.app.hiten.loanapplication.network;
+
+public interface ApiService {
+    // Register new user
+   /* @FormUrlEncoded
+    @POST("notes/user/register")
+    Single<User> register(@Field("device_id") String deviceId);
+ 
+    // Create note
+    @FormUrlEncoded
+    @POST("notes/new")
+    Single<Note> createNote(@Field("note") String note);
+ 
+    // Fetch all notes
+    @GET("notes/all")
+    Single<List<Note>> fetchAllNotes();
+ 
+    // Update single note
+    @FormUrlEncoded
+    @PUT("notes/{id}")
+    Completable updateNote(@Path("id") int noteId, @Field("note") String note);
+ 
+    // Delete note
+    @DELETE("notes/{id}")
+    Completable deleteNote(@Path("id") int noteId);*/
+}
